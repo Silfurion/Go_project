f5f92569edbc83376f25ff3a7a9bdeae3854a6d5
Bottet <nathan@lesbottet.net>
2020-10-25 20:04:19 +0100

    ajout de dijkstra

--- read_file.go
+++ read_file.go
@@ -13,7 +13,7 @@ import (
 func main() {
 
 
-var tab [][]string
+var tab [][]int
 var count = 0
 var length = 0
 
@@ -23,50 +23,62 @@ var length = 0
         fmt.Println("File reading error", err)
         return
     }
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan(){
+    reader := bufio.NewReader(file)
+     
+    for {
       if count == 0 {
-        count++
-        leng,err := strconv.ParseInt(scanner.Text(),10,32)
+        read, isPrefix, err:= reader.ReadLine()
+        if isPrefix {
+          fmt.Println("error")
+          break
+        }
+        if err != nil{
+          fmt.Println(err)
+          break
+        }
+        length,err = strconv.Atoi(string(read))
         if err != nil {
           fmt.Println(err)
-        }
-        length = int(leng)
-
-
-
-      
-      } else if count == length+1 {
           break
-      
-      } else{
-        fmt.Println(scanner.Text())
-        line :=strings.Split(scanner.Text(),";")
-        fmt.Println(line,len(line))
-        tab = append(tab,line)
+        }
         count++
       }
-
-  }
-  fmt.Println(tab,len(tab))
-  ConvertToInt(tab,length)
-    
-}
-func ConvertToInt(tab [][]string,length int){
-  var tab2 [][]float64
-  var intertab []float64
-  for i:=0;i<len(tab);i++{
-   for j:=0;j<len(tab);j++{
-      tabb,err := strconv.ParseFloat(tab[i][j],10)
-        if err!= nil {
-          fmt.Println(err)
+      if count < int(length)+1 {
+        read,isPrefix,err := reader.ReadLine()
+        if isPrefix  {
+          break
         }
-        intertab = append(intertab,tabb)
+        if err != nil {
+          fmt.Println(err)
+          break
+        }
+        intertab := strings.Split(string(read)," ")
+        intertabf := ConvertToInt(intertab)
+        tab = append(tab,intertabf)
+        if err != nil {
+          fmt.Println(err)
+          break
+        }
+       // tab = append(tab,intertab)
+        count++
+      }else {
+        break
+      }
+
+     }
+     fmt.Println(tab)
+}
+func ConvertToInt(tab []string) []int{
+  var intertab []int
+  for  i := 0 ; i<len(tab) ; i++ {
+    intertabf,err := strconv.Atoi(tab[i])
+    if err != nil {
+      fmt.Println(err)
     }
-      tab2 = append(tab2,intertab)
-      intertab = nil
-    }
-    fmt.Println(tab2)
+    intertab = append(intertab,intertabf)
+  }
+  return intertab
+  
 }
 
 
